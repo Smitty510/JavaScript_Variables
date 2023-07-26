946b1398c7ee0030d9991c7b98e225a963ab51ac
Smitty510 <smithpatrick.510@gmail.com>
2023-07-26 15:49:34 -0400

    Java

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Roland";
+let lastName = "Deschain";
+var age;
 
 // Exercise 2
+firstName = "Patrick"
+lastName = "Smith";
+age = 35;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
